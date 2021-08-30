4b791aee43afa56fbdb437a0455339de3e843ca0
Tahsin Ahmed <shanahmed1997@gmail.com>
2021-08-30 19:49:19 -0400

    added a respawn feature if the player dies, and made the player advance to the next level.

--- Project_Boost/Assets/Scripts/CollisionHandler.cs
+++ Project_Boost/Assets/Scripts/CollisionHandler.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
- using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
@@ -12,13 +12,9 @@ public class CollisionHandler : MonoBehaviour
             case "Friendly":
                 Debug.Log("This thing is friendly");
                 break;
-            //if the object's tag name that's being collided is named fuel
-            case "Fuel":
-                Debug.Log("You picked up fuel");
-                break;
             //if the object's tag name that's being collided is named finish
             case "Finish":
-                Debug.Log("You won the game");
+                LoadNextLevel();
                 break;
             //every other object that doesn't have either tags listed in the switch case
             default:
@@ -27,9 +23,24 @@ public class CollisionHandler : MonoBehaviour
         }
     }
 
+    void LoadNextLevel()
+    {
+        //it's the level the player is currently in
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        //go the next level by adding one to the scene index
+        int nextSceneIndex = currentSceneIndex + 1;
+        //if the player is in the last scene that was created
+        if(nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            //transport player back to scene index 0
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
     void ReloadLevel()
     {
-        //scene index 0 which is sandbox
+        //goes back to the level the player is currently in
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex);
     }
