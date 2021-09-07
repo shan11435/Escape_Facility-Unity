6ad66910cc0a2911a53f0a0d840562c458620c55
Tahsin Ahmed <shanahmed1997@gmail.com>
2021-09-06 21:07:20 -0400

    created a new script that makes any object go back and forth

---
+++ Project_Boost/Assets/Scripts/CameraFollow_Xaxis.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow_Xaxis : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+
+
+    
+ // Update is called once per frame
+ void Update() 
+ {
+      Vector3 position = transform.position ;
+      //this the line of code that makes the player only move up
+      position.x = (player.position + offset).x;
+      transform.position = position ;
+ }
+}
---
+++ Project_Boost/Assets/Scripts/Oscilator.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscilator : MonoBehaviour
+{
+    Vector3 startingPosition;
+    [SerializeField] Vector3 movementVector;
+    //[Range] gives a slider in unity
+    float movementFactor;
+    [SerializeField] float period = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //current position of the object in the world
+        startingPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this block of code is responsible for the objects moving back and forth, up and down, etc.
+        //continually growing over time
+        float cycles = Time.time / period;
+        
+        //constant value of 6.283
+        const float tau = Mathf.PI * 2;
+        //going from -1 to 1
+        float rawSinWave = Mathf.Sin(cycles * tau);
+        //instead of it going from -1 to 1 if we add 1, it will go from 0 to 2, then once divided by 2, it will go from 0 to 1
+        movementFactor = (rawSinWave + 1f) / 2f;
+
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPosition + offset;
+    }
+}
